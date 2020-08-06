50fc025c69b8104962cba47b97a5c7e77073a1da
Julien <julien.juret@gmail.com>
2020-08-06 14:21:06 +0200

    add axios system

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../organism/Header';
+import API from "../_helpers/api";
+
+// TODO : Use a library or a powerfull safemade error managment
 
 function Auth() {
+    const [loading, setLoading] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [alert, setAlert] = useState("");
+
     const _onSubmit = (ev: any) => {
         ev.preventDefault();
+        if (!email || !password) {
+            setAlert("Please enter valid credentials");
+            return;
+        }
+        setAlert("");
+        setLoading(true);
+        API.post("/auth", {
+            email,
+            password
+        }).then(res => {
+            setLoading(false);
+        }).catch(() => {
+            setLoading(false);
+            setAlert("Invalid Credentials");
+        })
     }
 
     return (
@@ -13,8 +36,23 @@ function Auth() {
                 <div className="row align-items-center justify-content-center">
                     <form className="col-md-7 mt-5 text-center" onSubmit={_onSubmit}>
                         <h2 className="text-center my-5">Authentication</h2>
-                        <input type="text" name="email" placeholder="email" className="form-control form-control-lg m-2" />
-                        <input type="password" name="password" placeholder="password" className="form-control form-control-lg m-2" />
+                        <input
+                            value={email}
+                            onChange={ev => setEmail(ev.target.value.trim())}
+                            type="text"
+                            name="email"
+                            placeholder="email"
+                            className="form-control form-control-lg m-2" />
+                        <input
+                            value={password}
+                            onChange={ev => setPassword(ev.target.value)}
+                            type="password"
+                            name="password"
+                            placeholder="password"
+                            className="form-control form-control-lg m-2" />
+                        {
+                            alert && <p className="alert alert-warning">{alert}</p>
+                        }
                         <button className="btn btn-outline-primary">Connect</button>
                     </form>
                 </div>

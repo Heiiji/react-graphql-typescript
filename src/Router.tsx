b991a950cbd3b82864e638383c06c47666f24eb0
Julien <julien.juret@gmail.com>
2020-08-09 18:12:12 +0200

    lock profil route

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,22 +4,28 @@ import Auth from "./pages/Auth";
 import Admin from "./pages/Admin";
 import RoomDetails from "./pages/RoomDetails";
 import { Switch, Route } from 'react-router-dom'
+import {useSelector} from "react-redux";
+import {getUser} from "./store/selectors";
 
 function Router() {
+    const user = useSelector(getUser);
+
   return (
       <Switch>
         <Route exact path="/" >
             <Home/>
         </Route>
-          <Route exact path="/auth" >
-              <Auth/>
-          </Route>
-          <Route exact path="/admin" >
-              <Admin/>
-          </Route>
+        <Route exact path="/auth" >
+            <Auth/>
+        </Route>
         <Route exact path="/room/:roomId" >
             <RoomDetails/>
         </Route>
+          {
+              user.role === 0 ? <Route exact path="/admin" >
+                  <Admin/>
+              </Route> : ""
+          }
       </Switch>
   );
 }

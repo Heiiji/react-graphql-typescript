134e5892340d28ee7fae363f1b7376af74544927
Julien <julien.juret@gmail.com>
2020-08-13 16:20:35 +0200

    REMOVE - useless reducers

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,15 +2,11 @@ import { combineReducers } from 'redux';
 import { userReducer } from './user/reducers';
 import { createStore } from 'redux';
 import { searchReducer } from './search/reducers';
-import { roomsReducer } from './rooms/reducers';
-import { propertiesReducer } from './property/reducers';
 import { bookingsReducer } from './bookings/reducers';
 
 export const rootReducer = combineReducers({
   search: searchReducer,
   user: userReducer,
-  rooms: roomsReducer,
-  properties: propertiesReducer,
   bookings: bookingsReducer,
 });
 
